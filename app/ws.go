c0de367bc95dbaa90b6d883e7572a1bb2ba53584
伏見尚人 <fushimi@wano.co.jp>
2019-08-28 10:50:44 +0900

    test

--- app/ws.go
+++ app/ws.go
@@ -1,11 +1,19 @@
 package app
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/gorilla/websocket"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	"github.com/labstack/gommon/log"
+	"net/http"
 	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"time"
 )
 
 var (
@@ -18,6 +26,8 @@ func NewWsApp() WsApp {
 
 	return &implWsApp {
 
+		wsPool: make( map[string]*websocket.Conn , 0),
+		fileInfoPool:make(map[string]*FileInfo , 0),
 	}
 }
 
@@ -28,6 +38,9 @@ type WsApp interface {
 
 type implWsApp struct {
 	e *echo.Echo
+
+	wsPool map[string]*websocket.Conn
+	fileInfoPool map[string]*FileInfo
 }
 
 func (self *implWsApp) Run() error {
@@ -39,7 +52,8 @@ func (self *implWsApp) Run() error {
 	fmt.Println(pwd)
 
 	e.Static("/", pwd + "/public")
-	e.GET("/ws", hello)
+	e.GET("/ws/start/:fileName", self.startFfprobeConnection)
+	e.GET("/get_data/:socketId/:fileName", self.getData)
 
 	self.e = e
 
@@ -53,30 +67,199 @@ func (self *implWsApp) Close() {
 	self.e.Close()
 }
 
+func (self *implWsApp) getData(c echo.Context) error {
 
-func hello(c echo.Context) error {
-	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
+	log.Info(`ffprobeからなんかきた`)
+
+
+	rangeReq := c.Request().Header.Get(`Range`)
+
+	//sId := c.Param(`socketId`)
+	fName := c.Param(`fileName`)
+	socketId := c.Param(`socketId`)
+
+	ws  , ok  := self.wsPool[socketId]
+	if !ok {
+		return c.String(http.StatusBadRequest , `no conn`)
+	}
+
+	info  , ok  := self.fileInfoPool[socketId]
+	if !ok {
+		return c.String(http.StatusBadRequest , `no conn`)
+	}
+
+	log.Info(rangeReq)
+
+	start , end , part , err := parseRangeHeader(rangeReq , info.Size)
 	if err != nil {
+		log.Error(err)
 		return err
 	}
-	defer ws.Close()
+
+	r := RangeRequest{
+		SocketId:socketId,
+		Range:rangeReq,
+		FileName:fName,
+		StartByte:start,
+		EndByte:end,
+	}
+	j   , _ := json.Marshal(r)
+
+
+	err = ws.WriteMessage(websocket.TextMessage, j)
+	if err != nil {
+		c.Logger().Error(err)
+	}
+
+	t, res, err := ws.ReadMessage()
+	if err != nil {
+		c.Logger().Error(err)
+	}
+
+	log.Info(t)
+
+	/*
+	res.set('Accept-Ranges', 'bytes');
+	res.set('Connection', 'close');
+	res.set('Content-Type', type);
+	res.set('Content-Length', range.end - range.start);
+	res.set('Content-Range', 'bytes ' + range.start + '-' + range.end + '/' + size);
+	res.statusCode = 206;
+	 */
+
+	if part != len(res) {
+		err = fmt.Errorf(`expected: %d , but: %d` , part , len(res))
+		log.Error(`sizeちゃうな`)
+		log.Error(err)
+		return err
+	}
+
+
+	c.Response().Header().Set(`Accept-Ranges`, `bytes`)
+	c.Response().Header().Set(`Connection`, `close`)
+	c.Response().Header().Set(`Content-Type`, info.Type)
+	c.Response().Header().Set(`Content-Length`, fmt.Sprint(len(res)))
+
+	rangeResponse := fmt.Sprintf(`bytes %d-%d/%d` , start , start + len(res) , info.Size)
+	fmt.Println(rangeResponse)
+	c.Response().Header().Set(`Content-Range` , rangeResponse)
+	c.Response().Status = http.StatusPartialContent
+
+	_ , err = c.Response().Write(res)
+	if err != nil {
+		log.Error(err)
+		return err
+	}
+
+	return nil
+}
+
+func parseRangeHeader(rangeHeader string , size int) (start int , end int , total int , err error){
+
+	defer func() {
+		if onPanic := recover(); onPanic != nil {
+			err = errors.New(fmt.Sprint(onPanic))
+			log.Error(err)
+		}
+	}()
+
+
+	b := strings.Split(rangeHeader , "=")
+	b2 := strings.Split(b[1] , "-")
+
+	_start := b2[0]
+	//_end := b2[1] // ""
+
+	s , err := strconv.Atoi(_start)
+	if err != nil {
+		log.Error(err)
+		return 0, 0, 0 , err
+	}
+
+	const partSize = 1024 * 1024 * 5
+
+	e := s + partSize
+	if e >= size {
+		e =  size
+	}
+
+	total = e - s
+
+	return s , e , total , nil
+}
+
+type RangeRequest struct {
+	SocketId  string `json:"socket_id"`
+	Range string `json:"range"`
+	FileName string `json:"file_name"`
+	StartByte int `json:"start_byte"`
+	EndByte int `json:"end_byte"`
+}
+
+type FileInfo struct {
+	Size int
+	Type string
+}
+
+
+func (self *implWsApp) startFfprobeConnection(c echo.Context) error {
+
+	size   , _ := strconv.Atoi(c.QueryParam(`size`))
+	fType := c.QueryParam(`file_type`)
+
+
+	uniq := fmt.Sprint(time.Now().UnixNano())
+
+	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
+	if err != nil {
+		log.Error(err)
+		return err
+	}
+
+	self.wsPool[uniq] =  ws
+	self.fileInfoPool[uniq]  = &FileInfo{
+		Size: size,
+		Type:fType,
+	}
+
+	defer func() {
+		ws.Close()
+		self.wsPool[uniq] =  nil
+		self.fileInfoPool[uniq] = nil
+	}()
+
+
+	fname := c.Param(`fileName`)
+
+	isComplete := false
+	go func() {
+
+		time.Sleep(time.Second)
+
+		log.Info(`ffprobeを起動`)
+		url := fmt.Sprintf( `http://127.0.0.1:1323/get_data/%s/%s` , uniq , fname)
+		cmd := fmt.Sprintf(`ffprobe -i '%s' -v quiet -print_format json -show_format -show_streams -show_error -show_chapters` , url)
+
+		fmt.Println(cmd)
+		out , err := exec.Command(`sh` , `-c` , cmd).CombinedOutput()
+		if err != nil {
+			log.Error(err)
+		}
+		isComplete = true
+
+		fmt.Println(string(out))
+	}()
 
 	for {
-		// Write
-		err := ws.WriteMessage(websocket.TextMessage, []byte("Hello, Client!"))
-		if err != nil {
-			c.Logger().Error(err)
-		}
+		time.Sleep(time.Second)
 
-		// Read
-		_, msg, err := ws.ReadMessage()
-		if err != nil {
-			c.Logger().Error(err)
+		if isComplete {
+			log.Info(`complete or error!`)
+			break
 		}
-		fmt.Printf("%s\n", msg)
 	}
+
+	return nil
 }
 
-func WS() {
 
-}
--- go.mod
+++ go.mod
@@ -6,7 +6,8 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/gorilla/websocket v1.4.1
 	github.com/labstack/echo v3.3.10+incompatible
-	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/labstack/gommon v0.3.0
+	github.com/sanity-io/litter v1.1.0
 	golang.org/x/crypto v0.0.0-20190820162420-60c769a6c586 // indirect
 	golang.org/x/net v0.0.0-20190404232315-eb5bcb51f2a3
 )
